71d86e577f704403f69f0a71273aee309fb62b8f
alexesmet <alexes.met@gmail.com>
2021-04-17 10:41:30 +0300

    Added feature for comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
@@ -29,17 +30,23 @@ def add_ingrid(name, order):
         mayo.find_element_by_css_selector("select.additionsize option:nth-child("+str(order)+")").click()
 
 
-def add_to_cart(address, qty=1, ingrids=[], lavash=None):
+def add_to_cart(address, qty=1, ingrids=[], lavash=None, comment=None):
     driver.get(address)
     btn = driver.find_element_by_css_selector("button.add-to-cart")
     btn.click()
     yes_delivery()
-    for each in ingrids:
-        add_ingrid(each[0], each[1])
-    for i in range(1,qty):
-        driver.find_element_by_id("qtyplus").click()
     if lavash:
         [label.click() for label in driver.find_elements_by_css_selector(".radio-container.spec") if label.text.startswith(lavash)]
+        sleep(0.5)
+    for each in ingrids:
+        add_ingrid(each[0], each[1])
+        sleep(0.5)
+    if comment:
+        driver.find_element_by_css_selector("input.order__comment").send_keys(comment)
+        sleep(0.5)
+    for i in range(1,qty):
+        driver.find_element_by_id("qtyplus").click()
+        sleep(0.1)
 
     driver.find_element_by_css_selector(".cart_continue button.delivery-type__addorder_btn").click()
     sleep(2)
@@ -54,6 +61,7 @@ def make_order():
     driver.find_element_by_css_selector("form#orderForm input[name=level]").send_keys("2")
     driver.find_element_by_css_selector("form#orderForm input[name=code]").send_keys("не работает")
     driver.find_element_by_css_selector("form#orderForm input[name=street]").send_keys("\n")
+    sleep(2)
     driver.find_element_by_css_selector("form#orderForm input[name=last_name]").send_keys("Метлицкий")
     driver.find_element_by_css_selector("form#orderForm input[name=first_name]").send_keys("Алексей")
     for d in list("447373590"): 
@@ -64,9 +72,17 @@ def make_order():
 
 if __name__ == "__main__":
     # add_to_cart("https://donerking.by/menu/banditos_s_kuritsey_xl")
-    # add_to_cart("https://donerking.by/menu/kurochka_bbq_s_ikroy_iz_zapechennyih_ovoschey_na_u_3")
-    add_to_cart("https://donerking.by/menu/shaurma_with_chicken_xxl", 2, [("Майонез", 2)], "Сырный")
     # add_to_cart("https://donerking.by/menu/doner_kebab_po_belarusski_xl")
+
+    add_to_cart("https://donerking.by/menu/kurochka_bbq_s_ikroy_iz_zapechennyih_ovoschey_na_u_3", 
+            comment="Без майонеза", 
+            ingrids=[("Шампиньоны жареные", "2")],
+            lavash="Сырный")
+
+    add_to_cart("https://donerking.by/menu/shaurma_with_chicken_xxl", 
+            qty=1, 
+            ingrids=[("Майонез", 2)], 
+            lavash="Сырный")
     make_order()
 
 
